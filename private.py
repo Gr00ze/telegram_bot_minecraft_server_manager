f2ab1e21e160fb7b21ab1fb44198d3c53318d2be
Gr00ze <84150091+Gr00ze@users.noreply.github.com>
2024-03-25 12:15:54 +0100

    Update private.py

--- private.py
+++ private.py
@@ -1,1 +1,8 @@
-TOKEN: str ="YOUR TOKEN"
+from telegram import User
+TOKEN : str ="YOUR TOKEN"
+
+user_whitelist : list[User] = []
+#User(id=123456,is_bot=False,username="username",first_name="can be empty")
+
+chat_id_whitelist : int = []
+#123456789
