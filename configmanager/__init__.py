3bed8bf880de9383dafc8a3ff96e84e1985ff42d
gr00ze <vincenzop23@gmail.com>
2025-04-13 04:17:32 +0200

    Modularized

---
+++ configmanager/__init__.py
@@ -0,0 +1,3 @@
+from configmanager.configmanager import ConfigManager
+
+__all__ = ['ConfigManager']
---
+++ telegram_polling_bot_minecraft_server_manager/__init__.py
@@ -0,0 +1,12 @@
+from telegram_polling_bot_minecraft_server_manager.server_manager import MCServerManager
+from telegram_polling_bot_minecraft_server_manager.log import log
+from telegram_polling_bot_minecraft_server_manager.server_message import ServerMessage
+
+
+# __init__.py
+
+# This file marks the directory as a Python package.
+# You can use this file to initialize the package or define package-level imports.
+
+
+__all__ = [ "MCServerManager","log","Authorizer","ServerMessage"]
